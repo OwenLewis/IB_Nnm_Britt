aa10e1f06178fe3d5c708405e348be98aec7535f
Owen Lewis <owenlewis@unm.edu>
2024-04-10 10:39:40 -0600

    Adding different BC to the gradient step
    I added new routines which compute the differentiation matrix for homogeneous dirichlet and neumann boundary conditions (in 1D).
    
    The actual gradient step now has switches based on the BCs in each direction, then applies them to rows/columns of the grid function as appropriate.
    
    This all assumes that the data is on a cell-centered grid.
    
    Note: this doesn't really matter for periodic, but matters a fair amount for Neumann and Dirichlet.

---
+++ src/centered_diff_nmn.m
@@ -0,0 +1,10 @@
+%
+% scaled centered difference matrix on a periodic domsin
+%
+function D1 = centered_diff_nmn(N);
+e  = ones(N,1);
+D1 = spdiags(0.5*[-e e],[-1 1],N,N);
+D1(1,1) = -0.5;
+D1(N,N) =  0.5;
+end
+
--- src/gradientFD.m
+++ src/gradientFD.m
@@ -1,13 +1,33 @@
 %
 % compute the gradient of a scalar function on a 2D domain using finite-differences
 %
-function Gu = gradient(u,grid)
+function Gu = gradientFD(u,grid)
 
-Dx = centered_diff_per(grid.Nx)/grid.dx;
-Dy = centered_diff_per(grid.Ny)/grid.dy;
+
+switch grid.bcx
+    case 'per'
+        Dx = centered_diff_per(grid.Nx)/grid.dx;
+    case 'dir'
+        Dx = centered_diff_dir(grid.Nx)/grid.dx;
+    case 'nmn'
+        Dx = centered_diff_nmn(grid.Nx)/grid.dx;
+end
+
+switch grid.bcy
+    case 'per'
+        Dy = centered_diff_per(grid.Ny)/grid.dy;        
+    case 'dir'
+        Dy = centered_diff_dir(grid.Ny)/grid.dy;
+    case 'nmn'
+        Dy = centered_diff_nmn(grid.Ny)/grid.dy;
+end
+        
+        
 
 Gu = zeros(grid.Nx,grid.Ny,2);
 Gu(:,:,1) = Dx*u;
 Gu(:,:,2) = u*Dy';
 
+
+end
 
